bb847c8ce597d948f73716bad80dea15c752e2b8
Luongpvph27222 <luongpvph27222@fpt.edu.vn>
2023-06-10 23:41:27 +0700

    update

---
+++ src/main/java/com/example/duanquanaojava5/Duanquanaojava5Application.java
@@ -0,0 +1,13 @@
+package com.example.duanquanaojava5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Duanquanaojava5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Duanquanaojava5Application.class, args);
+    }
+
+}
